7150e868dde93ac71636abdcb24b0c68363dbba1
ajaydewari <ajdewari@gmail.com>
2021-07-19 22:25:08 +0530

    RestaurantApi interface created for get request restaurant api

---
+++ app/src/main/java/me/ajay/networkcaching/api/RestaurantApi.kt
@@ -0,0 +1,15 @@
+package me.ajay.networkcaching.api
+
+import me.ajay.networkcaching.data.Restaurant
+import retrofit2.http.GET
+
+
+interface RestaurantApi {
+
+    companion object {
+        const val BASE_URL = "https://random-data-api.com/api/"
+    }
+
+    @GET("restaurant/random_restaurant?size=20")
+    suspend fun getRestaurants(): List<Restaurant>
+}
