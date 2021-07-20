59abfb654a88933a0bedc48bad3c04e76e5f1b73
ajaydewari <ajdewari@gmail.com>
2021-07-20 11:28:43 +0530

    Provider method added to AppModule to get the DB instance

--- app/src/main/java/me/ajay/networkcaching/di/AppModule.kt
+++ app/src/main/java/me/ajay/networkcaching/di/AppModule.kt
@@ -1,10 +1,13 @@
 package me.ajay.networkcaching.di
 
+import android.app.Application
+import androidx.room.Room
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import me.ajay.networkcaching.api.RestaurantApi
+import me.ajay.networkcaching.data.RestaurantDatabase
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -25,4 +28,10 @@ object AppModule {
     @Singleton
     fun provideRestaurantApi(retrofit: Retrofit): RestaurantApi =
         retrofit.create(RestaurantApi::class.java)
+
+    @Provides
+    @Singleton
+    fun provideDatabase(app: Application) : RestaurantDatabase =
+        Room.databaseBuilder(app, RestaurantDatabase::class.java, "restaurant_database")
+            .build()
 }
